e7d951869d580c083415d008e11824af21f26a99
andreas brunnström <best_i25@hotmail.com>
2020-10-28 20:57:22 +0100

    Added HandleBIN and HandleJSON aswell. - Andreas Brunnstrom

---
+++ src/FileConverter/Data/HandleBIN.cs
@@ -0,0 +1,33 @@
+﻿using FileConverter.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FileConverter.Data
+{
+    class HandleBIN : IFileHandler
+    {
+        public string Extension { get; } = ".bin";
+
+        public object Read(Stream fileStream)
+        {
+            try
+            {
+                BinaryFormatter binary = new BinaryFormatter();
+                var parsedBin = binary.Deserialize(fileStream) as List<ProductData>;
+                return parsedBin;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void Write(Stream stream, object data)
+        {
+            BinaryFormatter binary = new BinaryFormatter();
+            binary.Serialize(stream, data);
+        }
+    }
+}
---
+++ src/FileConverter/Data/HandleJSON.cs
@@ -0,0 +1,42 @@
+﻿using FileConverter.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileConverter.Data
+{
+    class HandleJSON : IFileHandler
+    {
+        public string Extension { get; } = ".json";
+
+        public object Read(Stream fileStream)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(fileStream))
+                using (JsonTextReader jsonReader = new JsonTextReader(reader))
+                {
+                    JsonSerializer ser = new JsonSerializer();
+                    return ser.Deserialize<List<ProductData>>(jsonReader);
+                }
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        public void Write(Stream stream, object data)
+        {
+            using (StreamWriter writer = new StreamWriter(stream))
+                using (JsonTextWriter jsonWriter = new JsonTextWriter(writer))
+            {
+                JsonSerializer ser = new JsonSerializer();
+                ser.Serialize(jsonWriter, data);
+                jsonWriter.Flush();
+            }
+        }
+    }
+}
